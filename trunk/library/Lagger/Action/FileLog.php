16323896daf3e95436ec35a8674b33ded0862aed
barbushin <barbushin@13156a46-be33-11de-8784-f7ad4773dbab>
2010-12-06 10:12:58 +0000

    Added: Lagger_Action_FileLog one-time mode

--- trunk/library/Lagger/Action/FileLog.php
+++ trunk/library/Lagger/Action/FileLog.php
@@ -1,33 +1,42 @@
 <?php
 
 /**
- * 
+ *
  * @see http://code.google.com/p/lagger
  * @author Barbushin Sergey http://www.linkedin.com/in/barbushin
- * 
+ *
  */
 class Lagger_Action_FileLog extends Lagger_Action {
-	
+
 	protected $filepath;
 	protected $template;
 	protected $sizeLimit;
 	protected $daysLimit;
-	
+	protected $onetimeMode;
+
 	const checkLimit = 100;
 
-	public function __construct($template, $filepath, $sizeLimit = null, $daysLimit = null) {
-		if(!file_exists($filepath)) { // required for realpath function works
+	public function __construct($template, $filepath, $sizeLimit = null, $daysLimit = null, $onetimeMode = false) {
+		if(!file_exists($filepath)) {
 			file_put_contents($filepath, '');
 		}
 		$this->filepath = realpath($filepath); // required for fopen works on script shutdown
+		$this->onetimeMode = $onetimeMode;
 		$this->template = $template;
 		$this->sizeLimit = (int) $sizeLimit;
 		$this->daysLimit = (int) $daysLimit;
 	}
 
 	protected function make() {
+		static $firstTime = true;
+		if($firstTime) {
+			if($this->onetimeMode) {
+				file_put_contents($this->filepath, '');
+			}
+			$firstTime = false;
+		}
+
 		$this->checkLimits();
-		
 		$fp = fopen($this->filepath, 'a'); // TODO: lock check/set
 		fputs($fp, $this->eventspace->fetch($this->template) . "\n");
 		fclose($fp);
@@ -36,12 +45,14 @@ class Lagger_Action_FileLog extends Lagger_Action {
 	protected function checkLimits() {
 		if(!mt_rand(0, self::checkLimit)) {
 			if($this->sizeLimit || $this->daysLimit) {
-				$fp = fopen($this->filepath, 'r');
-				$fstat = fstat($fp);
-				if($this->daysLimit && (time() - $fstat['mtime']) > ($this->daysLimit * 24 * 60 * 60)) {
-					unlink($this->filepath); // TODO: lock check/set, no unlink - just cut to 1/2 ?
+				if(file_exists($this->filepath)) {
+					$fp = fopen($this->filepath, 'r');
+					$fstat = fstat($fp);
+					fclose($fp);
+					if($this->daysLimit && (time() - $fstat['mtime']) > ($this->daysLimit * 24 * 60 * 60)) {
+						file_put_contents($this->filepath, '');
+					}
 				}
-				fclose($fp);
 			}
 		}
 	}
