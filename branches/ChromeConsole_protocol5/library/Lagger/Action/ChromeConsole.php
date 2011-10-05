321cc1fd01370a30395074ac2a15895c8ef33ded
barbushin <barbushin@13156a46-be33-11de-8784-f7ad4773dbab>
2011-10-05 19:31:59 +0000

    new ChromeConsole protocol draft

--- branches/ChromeConsole_protocol5/library/Lagger/Action/ChromeConsole.php
+++ branches/ChromeConsole_protocol5/library/Lagger/Action/ChromeConsole.php
@@ -6,36 +6,49 @@
  * @author Barbushin Sergey http://www.linkedin.com/in/barbushin
  *
  * @desc Sending messages to Google Chrome console
-
-You need to install Google Chrome extension:
-https://chrome.google.com/extensions/detail/nfhmhhlpfleoednkpnnnkolmclajemef
-
+ *
+ * You need to install Google Chrome extension PHP Console:
+ * https://chrome.google.com/webstore/detail/jafmfknfnkoekkdocjiaipcnmkklaajd
+ *
+ * All class properties and methods are static because it's required to let
+ * them work on script shutdown when FATAL error occurs.
+ *
  */
-
 class Lagger_Action_ChromeConsole extends Lagger_Action {
 
+	const serverProtocol = 4;
+	const cookieSizeLimit = 4000;
+	const messageLengthLimit = 1600;
+	const cookiesLimit = 50;
 	const clientProtocolCookie = 'phpcslc';
 	const serverProtocolCookie = 'phpcsls';
-	const serverProtocol = 4;
 	const messagesCookiePrefix = 'phpcsl_';
-	const cookiesLimit = 50;
-	const cookieSizeLimit = 4000;
-	const messageLengthLimit = 2500;
+	const clientPasswordCookie = 'phpcslp';
+	const serverAuthCookie = 'phpcsla';
 
-	protected static $isEnabledOnClient;
-	protected static $isDisabled;
+	protected static $isInitialized = false;
+	protected static $isEnabled = true;
+	protected static $requestUrl;
+	protected static $redirectUrl;
+	protected static $password;
+	protected static $processUid;
 	protected static $messagesBuffer = array();
-	protected static $bufferLength = 0;
-	protected static $messagesSent = 0;
 	protected static $cookiesSent = 0;
 	protected static $index = 0;
+
 	protected $stripBaseSourcePath = 0;
 
-	public function __construct($stripBaseSourcePath = null) {
-		if(self::$isEnabledOnClient === null) {
-			self::setEnabledOnServer();
-			self::$isEnabledOnClient = self::isEnabledOnClient();
-			if(self::$isEnabledOnClient) {
+	public function __construct($stripBaseSourcePath = null, $password = null) {
+		if(!self::$isInitialized) {
+			self::$isInitialized = true;
+			self::$processUid = mt_rand() . mt_rand();
+			self::$requestUrl = (stripos($_SERVER['SERVER_PROTOCOL'], 'https') === false ? 'http://' : 'https://') . (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME']) . $_SERVER['REQUEST_URI'];
+			if(strlen(self::$requestUrl) > 150) {
+				self::$requestUrl = substr(self::$requestUrl, 0, 147) . '...';
+			}
+			self::sendServerIsActive();
+			self::$isEnabled = self::isEnabledOnClient() && self::isValidPassword($password);
+			if(self::$isEnabled) {
 				ob_start();
 			}
 		}
@@ -44,90 +57,134 @@ class Lagger_Action_ChromeConsole extends Lagger_Action {
 		}
 	}
 
-	protected static function isEnabledOnClient() {
-		return isset($_COOKIE[self::clientProtocolCookie]) && $_COOKIE[self::clientProtocolCookie] == self::serverProtocol;
-	}
-
-	protected static function setEnabledOnServer() {
+	protected static function sendServerIsActive() {
 		if(!isset($_COOKIE[self::serverProtocolCookie]) || $_COOKIE[self::serverProtocolCookie] != self::serverProtocol) {
 			self::setCookie(self::serverProtocolCookie, self::serverProtocol);
 		}
 	}
 
+	protected static function isEnabledOnClient() {
+		return isset($_COOKIE[self::clientProtocolCookie]) && $_COOKIE[self::clientProtocolCookie] == self::serverProtocol;
+	}
+
+	protected static function isValidPassword($password) {
+		$isValidPassword = true;
+		if($password) {
+			$isValidPassword = isset($_COOKIE[self::passwordCookie]) && $_COOKIE[self::clientPasswordCookie] === $password;
+			self::setCookie(self::serverAuthCookie, $isValidPassword ? 'ok' : 'failed', true);
+		}
+		return $isValidPassword;
+	}
+
 	protected function make() {
-		if(!self::$isEnabledOnClient || self::$isDisabled) {
-			return;
-		}
-		$message['type'] = strpos($this->eventspace->getVarValue('tags'), 'error,') !== false ? 'error' : 'debug';
-		$message['subject'] = $this->eventspace->getVarValue('type');
-		$message['text'] = substr($this->eventspace->getVarValue('message'), 0, self::messageLengthLimit);
-
-		$file = $this->eventspace->getVarValue('file');
-		if($file) {
-			if($this->stripBaseSourcePath) {
-				$file = preg_replace('!^' . preg_quote($this->stripBaseSourcePath, '!') . '!', '', $file);
+		if(self::$isEnabled) {
+			$message = array(
+				'tags' => $this->eventspace->getVarValue('tags'),
+				'subject' => $this->eventspace->getVarValue('type'),
+				'text' => substr($this->eventspace->getVarValue('message'), 0, self::messageLengthLimit)
+			);
+			$file = $this->eventspace->getVarValue('file');
+			if($file) {
+				if($this->stripBaseSourcePath) {
+					$file = preg_replace('!^' . preg_quote($this->stripBaseSourcePath, '!') . '!', '', $file);
+				}
+				$line = $this->eventspace->getVarValue('line');
+				$message['source'] = $file . ($line ? ':' . $line : '');
 			}
-			$line = $this->eventspace->getVarValue('line');
-			$message['source'] = $file . ($line ? ":$line" : '');
-		}
 
-		$trace = $this->eventspace->getVarValue('trace');
-		if($trace) {
-			if($this->stripBaseSourcePath) {
-				$trace = preg_replace('!(#\d+ )' . preg_quote($this->stripBaseSourcePath, '!') . '!s', '\\1', $trace);
+			$trace = $this->eventspace->getVarValue('trace');
+			if($trace) {
+				if($this->stripBaseSourcePath) {
+					$trace = preg_replace('!(#\d+ )' . preg_quote($this->stripBaseSourcePath, '!') . '!s', '\\1', $trace);
+				}
+				$message['trace'] = explode("\n", $trace);
 			}
-			$message['trace'] = explode("\n", $trace);
-		}
-
-		self::pushMessageToBuffer($message);
-		if(strpos($this->eventspace->getVarValue('tags'), ',fatal')) {
-			self::flushMessagesBuffer();
+			self::pushMessageToBuffer($message);
+			/*
+			 * TODO: test if it'r really requried
+			if(strpos($this->eventspace->getVarValue('tags'), ',fatal')) {
+				self::flushMessagesBuffer();
+			}*/
 		}
 	}
 
 	protected static function pushMessageToBuffer($message) {
-		$encodedMessageLength = strlen(rawurlencode(json_encode($message)));
-		if(self::$bufferLength + $encodedMessageLength > self::cookieSizeLimit) {
-			self::flushMessagesBuffer();
-		}
 		self::$messagesBuffer[] = $message;
-		self::$bufferLength += $encodedMessageLength;
 	}
 
-	protected static function getNextIndex() {
+	protected static function getNextCookieIndex() {
 		return substr(number_format(microtime(1), 3, '', ''), -6) + self::$index++;
 	}
 
 	public static function flushMessagesBuffer() {
-		if(self::$messagesBuffer) {
-			self::sendMessages(self::$messagesBuffer);
-			self::$bufferLength = 0;
-			self::$messagesSent += count(self::$messagesBuffer);
-			self::$messagesBuffer = array();
-			self::$cookiesSent++;
-			if(self::$cookiesSent == self::cookiesLimit) {
-				self::$isDisabled = true;
-				$message = array('type' => 'error', 'subject' => 'PHP CONSOLE', 'text' => 'MESSAGES LIMIT EXCEEDED BECAUSE OF COOKIES STORAGE LIMIT. TOTAL MESSAGES SENT: ' . self::$messagesSent, 'source' => __FILE__, 'notify' => 3);
-				self::sendMessages(array($message));
+		if(self::$messagesBuffer && self::$isEnabled) {
+			$cookies = array();
+			$cookieMessages = array();
+			$cookieMessagesSize = 0;
+			foreach(self::$messagesBuffer as $message) {
+				$encodedMessageLength = strlen(rawurlencode(json_encode($message))) + 30;
+				if($cookieMessagesSize + $encodedMessageLength > self::cookieSizeLimit) {
+					$cookies[] = $cookieMessages;
+					$cookieMessages = array();
+					$cookieMessagesSize = 0;
+				}
+				$cookieMessages[] = $message;
+				$cookieMessagesSize += $encodedMessageLength;
 			}
+			self::$redirectUrl = self::getRedirectUrl();
+			$cookies[] = $cookieMessages;
+			foreach($cookies as $cookieMessages) {
+				if(self::$cookiesSent >= self::cookiesLimit) {
+					self::$isEnabled = false;
+					self::sendCookiesLimitExceeded();
+					break;
+				}
+				self::sendMessagesCookie($cookieMessages);
+				self::$cookiesSent++;
+			}
+			self::$messagesBuffer = array();
 		}
 	}
 
-	protected static function setCookie($name, $value, $temporary = false) {
+	protected static function sendCookiesLimitExceeded() {
+		self::sendMessagesCookie(array(array(
+				'type' => 'ahtung',
+				'subject' => 'PHP CONSOLE',
+				'text' => 'Cookies limit is exceeded. Try to increase limit or add messages filter.',
+				'source' => __FILE__ . ':' . __LINE__
+			)));
+	}
+
+	protected static function setCookie($name, $value, $isTemporary = false) {
 		if(headers_sent($file, $line)) {
-			throw new Exception('setcookie() failed because haders are sent (' . $file . ':' . $line . '). Try to use ob_start()');
+			die(__METHOD__ . ' error: setcookie() failed because headers was sent in ' . $file . ':' . $line . '. Try to use ob_start()');
 		}
 		setcookie($name, $value, null, '/');
-		if($temporary) {
+		if($isTemporary) {
 			setcookie($name, false, null, '/');
 		}
 	}
 
-	protected static function sendMessages($messages) {
-		self::setCookie(self::messagesCookiePrefix . self::getNextIndex(), json_encode($messages));
+	protected static function sendMessagesCookie(array $messages) {
+		$cookieId = self::getNextCookieIndex();
+		$data = array(
+			'PID' => self::$processUid,
+			'cookieId' => $cookieId,
+			'requestUrl' => self::$requestUrl,
+			'redirectUrl' => self::$redirectUrl,
+			'messages' => $messages
+		);
+		$cookieData = json_encode($data, defined('JSON_UNESCAPED_UNICODE') ? JSON_UNESCAPED_UNICODE : null);
+		self::setCookie(self::messagesCookiePrefix . $cookieId, $cookieData, true);
+	}
+
+	protected static function getRedirectUrl() {
+		return ($headers = headers_list()) && preg_match('/^Location\:\s*(.+)/i', end($headers), $matches) ? $matches[1] : null;
 	}
 
 	public function __destruct() {
 		self::flushMessagesBuffer();
 	}
 }
+
+
